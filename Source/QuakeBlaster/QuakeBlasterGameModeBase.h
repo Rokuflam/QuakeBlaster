06f7d783f5d6171a4bf118a2ef631e1a8202edf8
Roman <98953084+Rokuflam@users.noreply.github.com>
2023-02-23 20:09:29 +0200

    Initial commit

---
+++ Source/QuakeBlaster/QuakeBlasterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QuakeBlasterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUAKEBLASTER_API AQuakeBlasterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
